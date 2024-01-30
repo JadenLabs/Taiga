ccf9820366935f4d945a3f2bc31df6ca86d1c6b5
Jaden <100730821+JadenLabs@users.noreply.github.com>
2024-01-30 11:43:18 -0700

    feat(cmd): make progress on fishing animation

--- src/bot/commands/eco/fish.js
+++ src/bot/commands/eco/fish.js
@@ -2,17 +2,15 @@ const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const config = require("../../config");
 const logger = require("../../../utils/logger");
 const UserItem = require("../../../database/models/userItem");
+const { fishingAnimation } = require("../../../utils/fishing");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("fish")
         .setDescription(`Fish for... fish!`),
     async execute(interaction) {
-        // TODO: add functionality
+        await interaction.deferReply();
 
-        await interaction.reply({
-            content: "Under Construction",
-            ephemeral: true,
-        });
+        await fishingAnimation(interaction);
     },
 };
--- src/utils/fishing.js
+++ src/utils/fishing.js
@@ -1,10 +1,40 @@
 const { ButtonBuilder, ButtonStyle, EmbedBuilder } = require("discord.js");
 const config = require("../bot/config");
+const { sleep } = require("./time");
+const { getRandomValueFromArray } = require("./lists");
 
-async function handleFishing(interaction) {
-    // TODO: add functionality
+const ANIMATION_STEPS = 10;
 
-    const waitEmbed = new EmbedBuilder()
-        .setColor(config.colors.invis)
-        .setDescription(`FISHING PLACEHOLDER\n>\n>\n>VISUALS GO HERE\n>\n`);
+async function fishingAnimation(interaction) {
+    const progressBarLength = 20; // Adjust the length of the loading bar
+    const progressBarChar = "▓▓"; // Adjust the character used for the loading bar
+    const emptyProgressBarChar = "░░"; // Adjust the character used for the empty part of the loading bar
+
+    // Random message
+    const randomMessage = await getRandomValueFromArray(config.fishingMessages);
+
+    for (let i = 0; i < ANIMATION_STEPS; i++) {
+        // Make progress bar
+        const progress = ((i + 1) / ANIMATION_STEPS) * progressBarLength;
+        const progressBar = `${progressBarChar.repeat(
+            Math.floor(progress)
+        )}${emptyProgressBarChar.repeat(
+            Math.ceil(progressBarLength - progress)
+        )}`;
+
+        // Embed
+        const waitEmbed = new EmbedBuilder()
+            .setColor(config.colors.invis)
+            .setTitle("FISHING...")
+            .setDescription(
+                `WORK IN PROGRESS, DONT BULLY ME\n\`\`\`${progressBar}\`\`\``
+            )
+            .setFooter({ text: `${randomMessage}` });
+
+        await interaction.editReply({ embeds: [waitEmbed] });
+
+        await sleep(1000);
+    }
 }
+
+module.exports = { fishingAnimation };
