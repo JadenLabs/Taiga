91ad7e5ec02241e956bb6f3487afe8a2c3e9323a
Jaden <100730821+JadenLabs@users.noreply.github.com>
2024-02-04 20:59:13 -0700

    feat(util): implement bar emojis into fishing anim

--- src/utils/fishing.js
+++ src/utils/fishing.js
@@ -6,28 +6,30 @@ const { getRandomValueFromArray } = require("./lists");
 const ANIMATION_STEPS = 10;
 
 async function fishingAnimation(interaction) {
-    const progressBarLength = 20; // Adjust the length of the loading bar
-    const progressBarChar = "▓▓"; // Adjust the character used for the loading bar
-    const emptyProgressBarChar = "░░"; // Adjust the character used for the empty part of the loading bar
+    const progressBarLength = 8;
+    const progressBarChar = config.emojis.BMF;
+    const emptyProgressBarChar = config.emojis.BME;
 
     // Random message
     const randomMessage = await getRandomValueFromArray(config.fishingMessages);
 
     for (let i = 0; i < ANIMATION_STEPS; i++) {
         // Make progress bar
+        const endEmoji =
+            i == ANIMATION_STEPS - 1 ? config.emojis.BRF : config.emojis.BRE;
         const progress = ((i + 1) / ANIMATION_STEPS) * progressBarLength;
-        const progressBar = `${progressBarChar.repeat(
+        const progressBar = `${config.emojis.BLF}${progressBarChar.repeat(
             Math.floor(progress)
         )}${emptyProgressBarChar.repeat(
             Math.ceil(progressBarLength - progress)
-        )}`;
+        )}${endEmoji}`;
 
         // Embed
         const waitEmbed = new EmbedBuilder()
-            .setColor(config.colors.invis)
-            .setTitle("FISHING...")
+            .setColor(config.colors.primary)
+            .setTitle("Fishing...")
             .setDescription(
-                `WORK IN PROGRESS, DONT BULLY ME\n\`\`\`${progressBar}\`\`\``
+                `WORK IN PROGRESS, DONT BULLY ME\n## ${progressBar}`
             )
             .setFooter({ text: `${randomMessage}` });
 
