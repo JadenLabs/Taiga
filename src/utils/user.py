da4618d4c7681187ac07327d41b97e2e80286d6f
Jaden <jadenlabssocial@outlook.com>
2024-08-30 06:31:57 -0600

    fix(utils): fix id type mismatch in users
    edited from my phone, hoping this works

--- src/utils/user.py
+++ src/utils/user.py
@@ -9,7 +9,8 @@ DEFAULT_USER = {
 
 
 def find_user_or_default(id: int | str, default_user: dict = DEFAULT_USER):
-    user_doc = database.users.find_one({"_id": str(id)})
+    id = str(id)
+    user_doc = database.users.find_one({"_id": id})
     if user_doc is None:
         logger.debug(f"No doc for @{id}, creating")
         insert_doc = default_user
